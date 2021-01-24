d418593e89d0a7bdd16714cf44a006696c085581
toyu <20rmd21@ms.dendai.ac.jp>
2021-01-24 23:33:56 +0900

    Update environment.py
    add comment

--- environment.py
+++ environment.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 
+# バンディット問題の環境
 class Bandit(object):
+    # 腕の報酬確率を設定
     def __init__(self, k):
         while True:
             self.probability = np.asarray(np.random.rand(k))
@@ -13,19 +15,20 @@ class Bandit(object):
         sorted_prob = np.sort(self.probability)[::-1]
         self.opt_r = (sorted_prob[0] + sorted_prob[1]) / 2
 
-    #         print(self.probability)
-
+    # 報酬を返す
     def get_reward(self, selected):
         if self.probability[selected] >= np.random.rand():
             return 1
         else:
             return 0
 
+    # 正解の腕を選んだか判定する
     def get_correct(self, selected):
         if self.correct_arm == selected:
             return 1
         else:
             return 0
 
+    # regret を返す
     def get_regret(self, selected):
         return self.max_prob_arm - self.probability[selected]
